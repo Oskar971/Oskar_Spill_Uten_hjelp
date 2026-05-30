ecbc6157481af59a5ecfbf2a762284f35de4a6c7
Oskar971 <ossta007@osloskolen.no>
2026-05-30 14:07:40 +0200

    jjj
    hmmm

---
+++ Script.js
@@ -0,0 +1,14 @@
+document.getElementById("klikker").style.color = "red";
+
+let klikk = 10;
+
+const klikkBtn = document.getElementById("Klikks");
+const pick = document.getElementById("pick");
+
+function () {
+  KlikkBtn.innertext = "klikks: " + klikks + " 🖱️";
+
+  pick.onclick = function () {
+    klikks += 1;
+  };
+}
